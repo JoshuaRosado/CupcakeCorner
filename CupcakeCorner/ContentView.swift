16f5b7d8414b1baba8f1fa9173db8a85bc0f05e5
JoshuaRosado <joshua06rosado@gmail.com>
2025-01-23 12:49:37 -0600

    Documentation added to the lesson

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -49,27 +49,26 @@ struct ContentView: View {
         
     }
     
-    //Step 1
-        // Create URL we want to read from Apple's Service
-    // Step 2
-        // Fetch data from that URL using swift
-    //Step 3
-        // Decode that result into our Response Struct
-    
+
     func loadData() async {
         
-            // STEP 1
+        //Step 1
+            // Create URL we want to read from Apple's Service
+        
         guard let url = URL(string: "https://itunes.apple.com/search?term=bad+bunny&entity=song") else {
             print("Invalid URL")
             return
         }
         
-            // STEP 2
+        // Step 2
+            // Fetch data from that URL using swift
         do {
             // (data, discard response)
             let (data, _) = try await URLSession.shared.data(from:url)
             
-            //STEP 3
+            //Step 3
+                // Decode that result into our Response Struct
+            
             
             // We can decoded somehow...
             
