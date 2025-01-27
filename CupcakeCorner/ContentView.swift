ccf963e469e95a3082c06e34e9684c57c495988c
JoshuaRosado <joshua06rosado@gmail.com>
2025-01-27 15:44:50 -0600

    CoreHaptics

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -5,32 +5,49 @@
 //  Created by Joshua Rosado Olivencia on 1/22/25.
 //
 
+
+
+// Separate Framework
+import CoreHaptics // *** For Advanced haptics import CoreHaptics
 import SwiftUI
 
-@Observable
-class User: Codable {
-    // Fixing the coding key
-    // From "{"_name":"Cataleya","_$observationRegistrar":{}}"
-    // To {"name" : "Cataleya"}
-    // by replacing _name with "name"
-    enum CodingKeys : String, CodingKey {
-        case _name = "name"
-    }
-    var name = "Cataleya"
-}
-
 struct ContentView: View {
+    @State private var counter = 0
+    
+    // Create Haptics instance
+    @State private var engine: CHHapticEngine?
     
     var body: some View {
-        Button("Encode Cataleya", action:  encodeCataleya)
+        Button("Tap Count: \(counter)"){
+            counter += 1
+        }
+        //=================================================
+        // EASY way to customize haptics
+        .sensoryFeedback(.impact(flexibility: .soft, intensity: 0.5), trigger: counter)
+        //=================================================
+        
+        
+        // ADVANCED way to customize haptics
+        
+        // If we have Haptic, we start right away
+        // If can NOT start Haptic , put ERROR
     }
-    
-    func encodeCataleya() {
-        // data instance = try encoding a new User
-        let data = try! JSONEncoder().encode(User())
-        // str instance = displaying the data
-        let str = String(decoding: data, as: UTF8.self)
-        print(str)
+    func prepareHaptics() {
+        // If supports Haptics proceed, if not Exit
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        //DO BLOCK
+        do {
+            // Make our engine be a new CHHapticEnginer
+            engine = try CHHapticEngine()
+            //Then try starting engine
+            try engine?.start()
+            
+            // catch any error
+        } catch {
+            print("There was an error creating the engine: \(error.localizedDescription)")
+        }
+        
     }
 }
 
